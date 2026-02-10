a5706f7928c4eb202cbc008ff55da6b0d2816243
kazuki-dev <kazukit-dev@gmail.com>
2026-02-10 22:05:47 +0900

    chore(zsh): add comment

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -1,3 +1,4 @@
+# Create a directory and immediately change into it
 function dir() {
     mkdir "$1" && cd "$1"
 }
@@ -8,14 +9,26 @@ function dir() {
 # x: My experiments dirs
 # --------------------------------
 
+# Navigate to projects directory (~/i)
+# Usage: i [subdirectory]
 function i() {
     cd ~/i && cd "$1"
 }
 
+# Navigate to experiments directory (~/x)
+# Usage: x [subdirectory]
 function x() {
     cd ~/x && cd "$1"
 }
 
+# Clone a git repository into projects directory (~/i)
+# Usage: ic <git-url> [git-clone-options]
+function ic() {
+    cd ~/i && git clone "$@"
+}
+
+# Clone a git repository into experiments directory (~/x)
+# Usage: xc <git-url> [git-clone-options]
 function xc() {
-    cd ~/x && g clone "$@"
+    cd ~/x && git clone "$@"
 }
