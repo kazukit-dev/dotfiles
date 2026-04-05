d75fce463f3d79045dd9652dae04c3ecd5c14116
Kazukit <kazukit.dev@gmail.com>
2026-04-05 21:59:29 +0900

    chore(nvim): configure mini.statusline content
    Add custom active statusline content with mode, git status, diff, diagnostics, LSP info, filename, fileinfo, search count, and location display.

--- config/nvim/lua/plugins/mini-statusline.lua
+++ config/nvim/lua/plugins/mini-statusline.lua
@@ -3,6 +3,30 @@ return {
   {
     "echasnovski/mini.statusline",
     lazy = false,
-    opts = {},
+    opts = {
+      content = {
+        active = function()
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local git = MiniStatusline.section_git({ trunc_width = 40 })
+          local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+          local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+          local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+          local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+          local location = string.format("%d:%d", vim.fn.line("."), vim.fn.virtcol("."))
+
+          return MiniStatusline.combine_groups({
+            { hl = mode_hl, strings = { mode } },
+            { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+            "%<",
+            { hl = "MiniStatuslineFilename", strings = { filename } },
+            "%=",
+            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+            { hl = mode_hl, strings = { search, location } },
+          })
+        end,
+      },
+    },
   },
 }
