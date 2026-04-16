cd52c755be819ec4d7b9adc50c63f7cfdb85fc94
kazukit-dev <kazukit.dev@gmail.com>
2026-04-16 21:44:44 +0900

    chore(nvim): change lsp enable hook

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,7 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPost", "BufNewFile" },
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       vim.lsp.config("*", {
         capabilities = require("cmp_nvim_lsp").default_capabilities(),
